7fdc418d7c5b802573de370ec6ad4a3d30da6c1a
Janaymn <janaymnn@gmail.com>
2025-10-22 21:37:38 +0300

    updated lgbm

--- train_autolog_lgbm.py
+++ train_autolog_lgbm.py
@@ -1,12 +1,13 @@
 # train_autolog_lgbm.py
 
+import os
 import pandas as pd
 from sklearn.metrics import precision_score, recall_score, f1_score
 import lightgbm as lgb
 import mlflow
 from dotenv import load_dotenv
 
-# Load .env (for DagsHub MLflow remote)
+# Load Dagshub MLflow environment variables
 load_dotenv()
 
 # Enable autologging for LightGBM
@@ -24,7 +25,10 @@ y_train = train["species"]
 X_test = test.drop("species", axis=1)
 y_test = test["species"]
 
-# Start MLflow run
+# Create folder for DVC output
+os.makedirs("models_lgbm", exist_ok=True)
+
+# Train and log
 with mlflow.start_run(run_name="LightGBM Autolog"):
     model = lgb.LGBMClassifier(random_state=42)
     model.fit(X_train, y_train)
@@ -34,5 +38,8 @@ with mlflow.start_run(run_name="LightGBM Autolog"):
     mlflow.log_metric("precision", precision_score(y_test, y_pred, average='macro'))
     mlflow.log_metric("recall", recall_score(y_test, y_pred, average='macro'))
     mlflow.log_metric("f1_score", f1_score(y_test, y_pred, average='macro'))
+    
+    # Save model manually for DVC tracking
+    model.booster_.save_model("models_lgbm/lgbm_model.txt")
 
-print("LightGBM autolog run logged successfully on Penguins dataset.")
+print(" LightGBM autolog run logged successfully on Penguins dataset.")
